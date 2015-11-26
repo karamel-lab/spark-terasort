862e8f4011fd12c33fe735fee3be2e7728502cf5
Shelan Perera <shelanrc@gmail.com>
2015-11-26 17:42:17 +0100

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -7,7 +7,7 @@ script 'run_experiment' do
   code <<-EOM
 wget https://s3-eu-west-1.amazonaws.com/karamel/terasort_2.10-0.0.1.jar
 
-/srv/spark-1.3.1-bin-hadoop2.4/bin/spark-submit --master spark://172.28.128.5:7077 --class eastcircle.terasort.SparkTeraSort /tmp/terasort_2.10-0.0.1.jar hdfs://172.28.128.5:8020 /srv/teragen/ /terasortOut 1
+/srv/spark-1.3.1-bin-hadoop2.4/bin/spark-submit --master spark://#{node.spark_terasort.experiment.public_ips[0]}:7077 --class eastcircle.terasort.SparkTeraSort /tmp/terasort_2.10-0.0.1.jar hdfs://#{node.spark_terasort.experiment.public_ips[0]}:8020 /srv/teragen/ /terasortOut 1
   EOM
 end
 
