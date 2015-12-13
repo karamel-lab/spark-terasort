eef2988b8afae19ccfd492c33f37d9d3a465efde
Ashansa Perera <ashansa.perera@gmail.com>
2015-12-13 22:37:47 +0100

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -6,9 +6,9 @@ script 'run_experiment' do
   interpreter "bash"
   code <<-EOM
 wget https://s3-eu-west-1.amazonaws.com/karamel/terasort_2.10-0.0.1.jar
-echo "$(date)" > /user/#{spark_terasort.user}/sparkTime1.txt
+echo "$(date)" > /home/#{spark_terasort.user}/sparkTime1.txt
 /srv/spark-1.3.1-bin-hadoop2.4/bin/spark-submit --master spark://#{node.spark_terasort.experiment.private_ips[0]}:7077 --class eastcircle.terasort.SparkTeraSort /tmp/terasort_2.10-0.0.1.jar hdfs://#{node.spark_terasort.experiment.private_ips[0]}:8020 /srv/teragen/ /terasortOut 1
-echo "$(date)" > /user/#{spark_terasort.user}/sparkTime2.txt  
+echo "$(date)" > /home/#{spark_terasort.user}/sparkTime2.txt  
   EOM
 end
 
